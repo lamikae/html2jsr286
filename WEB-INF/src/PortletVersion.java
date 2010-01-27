34a376e1e96aa49939a52ccb7f7b45dbdf81e0f7
lamikae <lamikae@d8991cde-04a0-4fdb-8b94-cc33c1e904e7>
2010-01-27 08:25:09 +0000

    version 0.9.1

--- WEB-INF/src/PortletVersion.java
+++ WEB-INF/src/PortletVersion.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2009 Mikael Lammentausta
+ * Copyright (c) 2009,2010 Mikael Lammentausta
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,6 +24,6 @@ package com.celamanzi.liferay.portlets.rails286;
 
 public interface PortletVersion
 {
-	public static final String PORTLET_VERSION = "0.9.0";
+	public static final String PORTLET_VERSION = "0.9.1";
 	public static final int[]  LIFERAY_VERSION = { 5,2 }; // supported Liferay version
 }
